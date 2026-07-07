2b1c8515ecd610ebb99c66ef0f036f525c4503af
leo98lxl <leo.leksell@live.se>
2026-07-07 16:27:54 +0200

    styling nav bar continued

--- components/main_nav.tsx
+++ components/main_nav.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function MainNav() {
     return (
-        <header className="flex flex-wrap flex-col gap-[1rem] md:flex-row px-[1rem]">
-            <Link href="/">Home</Link>
-            <nav className="flex flex-wrap flex-col gap-[1rem] md:flex-row">
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/profile">Profile</Link>
+        <header className="flex flex-wrap flex-col gap-[1rem] md:flex-row px-[1rem] py-[1rem] bg-gray-700">
+            <nav className="flex flex-wrap flex-col gap-[1rem] content-between md:flex-row text-cyan-500">
+                <Link className="font-bold text-cyan-500" href="/">Home</Link>
+                <Link className="" href="/about">About</Link>
+                <Link className="" href="/contact">Contact</Link>
+                <Link className="" href="/profile">Profile</Link>
             </nav>
         </header>
     )
