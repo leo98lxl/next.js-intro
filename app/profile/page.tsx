c5fcb82e267eef33105261988e25dc471600bf35
leo98lxl <leo.leksell@live.se>
2026-07-07 14:59:49 +0200

    added picture and profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import MainNav from "@/components/main_nav"
+
+export default function Home() {
+  return (
+    <div>
+        <MainNav>
+        </MainNav>
+        <p>This is the Profile page.</p>
+    </div>
+  );
+}
