0a7089a3169e470e00d8bcfa9e618bbe62c1bf78
leole <leo.leksell@live.se>
2026-07-08 15:23:07 +0200

    cat facts styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,20 @@
-import MainNav from "@/components/main_nav"
+import MainNav from "@/components/main_nav";
+import data from "@/data/fact.json";
 
-export default function Home() {
+export default function Contact() {
   return (
     <div>
         <MainNav>
         </MainNav>
-        <p>This is the Contact page.</p>
+        <h1 className="p-4 font-bold text-center">This is the Contact page.</h1>
+        <section>
+          <ul>
+            <li>
+              <h3 className="p-4 font-bold text-cyan-500">Cat fact of the day: <span className="font-normal">{data.fact}</span></h3>
+              <p className="p-4 font-bold text-cyan-500">Average age of cat owner: <span className="font-normal">{data.length}</span></p>
+            </li>
+          </ul>
+        </section>
     </div>
   );
 }
