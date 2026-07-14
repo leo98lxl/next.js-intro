9f09c0726b14af721c304fd34cc9d8dccb3977d2
leole <leo.leksell@live.se>
2026-07-14 15:39:31 +0200

    added and displayed PokeAPI as GraphQl

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,6 +12,30 @@ interface CatFacts {
   length: number;
 }
 
+interface PokemonQl {
+  name: string;
+  id: number;
+}
+
+interface PokemonQlResponse {
+  data?: {
+    gen3_species?: Array<{
+      name: string;
+      id: number;
+    }>;
+  };
+  errors?: Array<{ message: string }>;
+}
+
+function DisplayPokemonQl ({name, id}: PokemonQl) {
+  return (
+    <article>
+      <h3 className="font-extrabold uppercase py-4">{name}</h3>
+        <p className="py-4 text-cyan-500">ID: {id}</p>
+    </article>
+  );
+}
+
 function CatCard({fact, length}: CatFacts) {
   return (
     <article>
@@ -21,13 +45,41 @@ function CatCard({fact, length}: CatFacts) {
   )
 }
 
-export default function Contact() {
+export default async function Contact() {
+  const pokeQl = `query samplePokeAPIquery {
+  gen3_species: pokemonspecies(
+    where: {generation: {name: {_eq: "generation-iii"}}}
+    order_by: {id: asc}
+  ) {
+    name
+    id
+  }
+  generations: generation {
+    name
+    pokemon_species: pokemonspecies_aggregate {
+      aggregate {
+        count
+      }
+    }
+  }
+}`;
+
+  const fetchedPokeQl = await fetch("https://graphql.pokeapi.co/v1beta2/", { method: "POST", headers: {
+  "Content-Type": "application/json" }, body: JSON.stringify({ query: pokeQl }), });
+  if (!fetchedPokeQl.ok) throw new Error("Failed to load Pokémon!");
+
+  const jsonPokeQl: PokemonQlResponse = await fetchedPokeQl.json();
+  const pokemonList = jsonPokeQl.data?.gen3_species ?? [];
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Image src="/IMG_4155.jpg" width={800} height={200} loading="lazy" alt="Picture of movie collection"></Image>
       <section>
       <h1 className="p-4 font-bold text-center">This is the Contact page.</h1>
         <CatCard fact={data.fact} length={data.length} />
+
+        {pokemonList.map((pokemon) => (
+        <DisplayPokemonQl key={pokemon.name} name={pokemon.name} id={pokemon.id} /> ))}
       </section>
     </div>
   );
