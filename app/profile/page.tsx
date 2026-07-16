4888e59c7fc60e8e23eea75506f314f04abcb54e
leole <leo.leksell@live.se>
2026-07-16 16:03:43 +0200

    UI fixes

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -20,7 +20,7 @@ export default async function Profile({searchParams}: {searchParams: Promise<{[k
         <div className="grid col-span-full gap-4 text-base font-medium sm:flex-row">
                   <Form action="/profile">
                   <input className="outline-2" name="query" />
-                  <button className="p-4 text-cyan-500 hover:text-cyan-200 focus-visible:text-cyan-200 hover:cursor-pointer transition duration-150" type="submit">Search</button>
+                  <button className="p-4 text-cyan-500 hover:text-cyan-200 focus-visible:text-cyan-200 hover:cursor-pointer transition duration-150" type="submit">Search (WIP)</button>
                   </Form>
                   <DisplayPokemonCard currentPage={currentPage} pageLimit={pageLimit} query={`${query}`}>
                   </DisplayPokemonCard>
--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -8,9 +8,9 @@ interface Pokemon {
 
 function DisplayPokemon({results}: {results: Pokemon}) {
   return (
-    <article className="grid">
-      <h3 className="font-extrabold text-center uppercase p-4">{results.name}</h3>
-      <p className="py-4 text-cyan-500 underline">{results.url}</p>
+    <article className="grid text-center">
+      <h3 className="font-extrabold uppercase p-4">{results.name}</h3>
+      <Link className="py-4 text-cyan-500 underline" href={results.url}>View details (JSON)</Link>
     </article>
   )
 }
@@ -28,10 +28,10 @@ export default async function DisplayPokemonCard({currentPage, pageLimit, query,
             <p>Total Pokémon: <span className="p-4 text-cyan-500">{count}</span></p>
 
             {previousPokePage && <Link className="p-4 hover:text-cyan-200 focus-visible:text-cyan-200 hover:cursor-pointer transition duration-150" 
-            href={ {pathname: "/profile", query: {page: currentPage - 1 }}}>Previous Page</Link>}
+            href={ {pathname: "/profile", query: {page: currentPage - 1 }}}>Previous 20 Pokémon</Link>}
 
             {nextPokePage && <Link className="p-4 hover:text-cyan-200 focus-visible:text-cyan-200 hover:cursor-pointer transition duration-150"
-            href={ {pathname: "/profile", query: {page: currentPage + 1 }}}>Next Page</Link>}
+            href={ {pathname: "/profile", query: {page: currentPage + 1 }}}>Next 20 Pokémon</Link>}
             </div>
         </div>
     )  
